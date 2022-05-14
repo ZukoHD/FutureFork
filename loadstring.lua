c533d2466312c258ec07aee5eb31c162de5b24a1
dori <69031971+ZukoHD@users.noreply.github.com>
2022-05-14 22:26:00 +1000

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,6 +1,6 @@
-if shared.FutureDeveloper then 
-    loadfile("Future/Initiate.lua")()
+if shared.Future_ForkDeveloper then 
+    loadfile("Future_Fork/Initiate.lua")()
     return
 end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/joeengo/Future/main/Initiate.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZukoHD/FutureFork/main/Initiate.lua"))()
